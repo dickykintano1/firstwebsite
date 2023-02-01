518a6c41d2145b896bb5aa5b61740faac4cba399
dickykintano1 <60821382+dickykintano1@users.noreply.github.com>
2023-02-01 21:50:53 +0700

    Putting Attributes on new page
    Putting Attributes on Create Recipe Page

--- FrontEnd/cook-web/src/components/pages/CreateRecipe.js
+++ FrontEnd/cook-web/src/components/pages/CreateRecipe.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import '../css/CreateRecipe.css';
+import '../css/General.css';
 
 function CreateRecipe() {
   return (
-    <div></div>
+    <html className='html-create-recipe'>
+      <form className='input-container-create-recipe' action=''>
+        <p className='text-create-recipe1'>Share your recipe to the world!</p>
+        {/* <hr className='general-line'></hr> */}
+        <p className='text-create-recipe2'>Name of the food:</p>
+        <input
+          className='input-create-recipe1'
+          type='food-name'
+          placeholder='What is it called?'
+        />
+        <p className='text-create-recipe2'>Cooking steps:</p>
+        <input
+          className='input-create-recipe2'
+          type='food-steps'
+          placeholder='Cooking steps here!'
+        />
+        <button
+          className='button-create-recipe'
+          type='submit'
+        >Submit Recipe</button>
+      </form>
+    </html>
   )
 }
 
